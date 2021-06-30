bf6d1b671fa97e4a3c664945c492e468bd53e712
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-30 08:37:52 +0530

    [Afrath] Updated the file with fibonacci program

---
+++ Logical Programming/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming
+{
+    /// <summary>
+    /// Fibonacci Series Program.
+    /// </summary>
+    class Fibonacci
+    {
+        public static void FibonacciSeries()
+        {
+            Console.WriteLine("Enter the Number of elements in series: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int fib1 = 0, fib2 = 1, sum = 0;
+            Console.Write("{0} {1}",fib1,fib2);
+            for(int i = 2; i < N; i++)
+            {
+                sum = fib1 + fib2;
+                Console.Write(" "+sum);
+                fib1 = fib2;
+                fib2 = sum;
+            }
+            Console.WriteLine(" ");
+
+
+        }
+    }
+}
--- Logical Programming/Program.cs
+++ Logical Programming/Program.cs
@@ -7,6 +7,21 @@ namespace Logical_Programming
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to logical programming!");
+            while (true)
+            {
+                Console.WriteLine("Choose an Option: ");
+                Console.WriteLine("Enter 1 for Finonacci Series / Enter 2 to Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Fibonacci.FibonacciSeries();
+                        break;
+                    case 2:
+                        return;
+                        break;
+                }
+            }
         }
     }
 }
