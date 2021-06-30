4ae08ceea7bdb517a5f641383351d8625d21c7a4
MOHAMEDAfrath <afrathismath@gmail.com>
2021-06-30 09:08:34 +0530

    [Afrath] Updated the file with Perfect number program

---
+++ Logical Programming/PerfectNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming
+{
+    class PerfectNumber
+    {
+        public static void CheckPerfectNumber()
+        {
+            Console.WriteLine("Enter to number to check perfect number:");
+            int perfect = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for(int i = 1; i < perfect; i++)
+            {
+                if (perfect % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            string result = sum == perfect ? " is a perfect number" : " is not a perfect number";
+            Console.WriteLine(perfect+result);
+        }
+    }
+}
--- Logical Programming/Program.cs
+++ Logical Programming/Program.cs
@@ -10,7 +10,8 @@ namespace Logical_Programming
             while (true)
             {
                 Console.WriteLine("Choose an Option: ");
-                Console.WriteLine("Enter 1 for Finonacci Series / Enter 2 to Exit");
+                Console.WriteLine("Enter 1 for Finonacci Series");
+                Console.WriteLine("Enter 2 for Perfect Number");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -18,6 +19,11 @@ namespace Logical_Programming
                         Fibonacci.FibonacciSeries();
                         break;
                     case 2:
+                        PerfectNumber.CheckPerfectNumber();
+                        break;
+
+
+                    case 5:
                         return;
                         break;
                 }
